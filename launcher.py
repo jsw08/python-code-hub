ce1d2ccda6d1f6bbd8f8e2e8c7184d10887c321a
wh4ky <damian.czapnik360@outlook.com>
2023-11-21 20:52:44 +0100

    folders are marked and small changes

--- launcher.py
+++ launcher.py
@@ -1,17 +1,31 @@
 import os
 
-while True:
-    programs = os.listdir("./lib")
-    print("Choose a program you want to run: ")
-    for element in programs:
-        print(str(element))
-    print("")
+def launcher():
+    while True:
+        programs = os.listdir("./lib")
+        folders = []
+        print("Choose a program you want to run: ")
+        for program in programs:
+            if os.path.isdir("./lib/" + program) == True:
+                print("(folder) " + program)
+                folders.append(program)
+            if program in folders:
+                print
+            else:
+                print(program)
+        print()
 
-    program_to_run = input()
-    if program_to_run not in programs:
-        print("Not a valid program.")
+        program_to_run = input()
+        if program_to_run not in programs:
+            print("Not a valid program.")
     
-    else:
-        print("Running program.")
-        os.system("python ./lib/" + program_to_run)
-        break
+        elif program_to_run in folders:
+            new_programs = os.listdir("./lib/" + program_to_run)
+            print(new_programs)
+            
+        else:
+            print("Running program.")
+            os.system("python ./lib/" + program_to_run)
+            break
+
+launcher()
