35a3bd900641f1e0e2616d246c45844bb2a9c4de
wh4ky <damian.czapnik360@outlook.com>
2023-11-22 10:07:41 +0100

    can run code in a directory

--- launcher.py
+++ launcher.py
@@ -1,5 +1,36 @@
 import os
 
+def dir():
+    while True:
+        directories = []
+        directories.append()
+        folders = []
+        dir_programs = os.listdir("./lib/" + program_to_run)
+        print("What program in directory ./lib/" + program_to_run + " do you want to run?")
+        for dir_program in dir_programs:
+            if os.path.isdir("./lib/" + program_to_run + "/" + dir_program) == True:
+                print("(dir) " + dir_program)
+                folders.append(dir_program)
+            if dir_program in folders:
+                print
+            else:
+                print(dir_program)
+        print()
+
+        new_program_to_run = input()
+        if new_program_to_run not in dir_programs:
+            print("Not a valid program or directory.")
+            
+        elif new_program_to_run in folders:
+            dir_programs = os.listdir("./lib/" + program_to_run + "/" + new_program_to_run)
+            dir()
+
+        else:
+            print("Running program.")
+            os.system("python ./lib/" + program_to_run + "/" + new_program_to_run)
+            break
+
+
 def launcher():
     while True:
         programs = os.listdir("./lib")
@@ -7,7 +38,7 @@ def launcher():
         print("Choose a program you want to run: ")
         for program in programs:
             if os.path.isdir("./lib/" + program) == True:
-                print("(folder) " + program)
+                print("(dir) " + program)
                 folders.append(program)
             if program in folders:
                 print
@@ -15,13 +46,14 @@ def launcher():
                 print(program)
         print()
 
+        global program_to_run
         program_to_run = input()
         if program_to_run not in programs:
-            print("Not a valid program.")
+            print("Not a valid program or directory.")
     
         elif program_to_run in folders:
-            new_programs = os.listdir("./lib/" + program_to_run)
-            print(new_programs)
+            dir_programs = os.listdir("./lib/" + program_to_run)
+            dir()
             
         else:
             print("Running program.")
